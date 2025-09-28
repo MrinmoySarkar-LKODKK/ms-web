1fb6333950e49fb7c79f384318693842dedfa6c7
MrinmoySarkar-LKODKK <sarkarmrinmoy99@gmail.com>
2025-09-28 21:33:24 +0530

    test PHP's Heredoc syntax

--- includes/header-head.php
+++ includes/header-head.php
@@ -1,7 +1,18 @@
+<?php
+    $header_part1 = <<<HTML
+        <meta charset="UTF-8">
+        <!-- mobile responsive meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    HTML;
 
-	<title>ললিত কলা ও দৃশ্যকলা কেন্দ্র</title>
 
 
+    $header_part2 = <<<HTML
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
 
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/responsive.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
+    HTML;
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php 
+    include 'includes/header-head.php'; 
+    echo $header_part1;
+    ?>
+
+	<title>ললিত কলা ও দৃশ্যকলা কেন্দ্র</title>
+    <?php 
+    echo $header_part2;
+    ?>
+</head>
+<body>
+    <div class="page-wrapper">
+    </div>
+</body>
+</html>
+
